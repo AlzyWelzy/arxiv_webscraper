68d62a4421fa62eeee601426e42257e5c6f6e4a9
AlzyWelzy <welzyalzy@gmail.com>
2023-05-11 11:09:44 +0530

    done

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# arxiv_webscrapper
-Arxiv Web Scrapper
+# ArxivScraper
+
+This is a Python class that scrapes research papers from the arXiv database based on a user's query and allows them to download a paper of their choice.
+
+## Requirements
+- Python 3.11 or higher
+- requests library
+- BeautifulSoup library
+
+## Installation
+1. Clone the repository
+2. Install the requests library using the command `pip install requests`
+3. Install the BeautifulSoup library using the command `pip install beautifulsoup4`
+4. Run the script using the command `python3 main.py`
+
+## Usage
+1. Enter your query
+2. Enter the index of the paper you want to download
+
+## License
+[AGPL-3.0](https://choosealicense.com/licenses/agpl-3.0/)
+
+## Author
+[AlzyWelzy](https://github.com/AlzyWelzy)
--- main.py
+++ main.py
@@ -11,68 +11,62 @@ class ArxivScraper:
         self.researches = None
         self.index = None
 
-    # function to get the query from the user
     def get_query(self):
         self.query = input("Enter your query: ")
 
-    # function to get the url of the query
     def get_url(self):
         self.url = "https://arxiv.org/search/?query=" + self.query + "&searchtype=all&source=header"
 
-    # function to get the html of the url
     def get_html(self):
         self.html = requests.get(self.url)
 
-    # function to get the soup of the html
     def get_soup(self):
         self.soup = BeautifulSoup(self.html.text, "html.parser")
 
-    # function to get the list of all the researches
     def get_researches(self):
         self.researches = self.soup.find_all("li", class_="arxiv-result")
 
-    # function to get the title of the research
     def get_title(self, research):
         title = research.find("p", class_="title is-5 mathjax").text
         return title
 
-    # function to get the authors of the research
     def get_authors(self, research):
         authors = research.find("p", class_="authors").text
         return authors
 
-    # function to get the abstract of the research
     def get_abstract(self, research):
         abstract = research.find("span", class_="abstract-full has-text-grey-dark mathjax").text
         return abstract
 
-    # function to get the pdf link of the research
     def get_pdf_link(self, research):
         pdf_link = research.find("a", class_=None)["href"].replace("abs", "pdf")
         return pdf_link
 
-    # function to show the results to the user
     def show_results(self):
         for i, research in enumerate(self.researches):
-            print(f"{i+1}. {self.get_title(research)}")
+            print(f"{i + 1}. {self.get_title(research)}")
             print(f"   {self.get_authors(research)}")
             print(f"   {self.get_abstract(research)}")
             print(f"   {self.get_pdf_link(research)}")
             print()
 
-    # function to get the index of the research the user wants to download
     def get_index(self):
         self.index = int(input("Enter the index of the research you want to download: "))
 
-    # function to download the pdf file
     def download_pdf(self):
-        pdf_link = self.get_pdf_link(self.researches[self.index - 1])
-        pdf = requests.get(pdf_link)
-        with open(f"{self.get_title(self.researches[self.index-1])}.pdf", "wb") as f:
-            f.write(pdf.content)
-        print("File downloaded successfully")
+        title = self.get_title(self.researches[self.index - 1]).strip()
 
-    # function to run the program
+        title = title.replace(" ", "_")
+
+        pdf_link = self.get_pdf_link(self.researches[self.index - 1])
+
+        pdf = requests.get(pdf_link)
+
+        with open(title + ".pdf", "wb") as f:
+            f.write(pdf.content)
+
+        print("Downloaded Successfully!")
+
     def run(self):
         self.get_query()
         self.get_url()
